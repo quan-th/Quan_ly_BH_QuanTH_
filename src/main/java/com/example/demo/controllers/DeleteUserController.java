91fc6023cd79662aad647f7df187774649563ab3
tranhongquan <tranhongquanutcit3@gmail.com>
2017-06-30 15:11:33 +0700

    test ommit

--- src/main/java/com/example/demo/controllers/DeleteUserController.java
+++ src/main/java/com/example/demo/controllers/DeleteUserController.java
@@ -4,17 +4,11 @@
  */
 package com.example.demo.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.logics.TblInsuranceLogic;
 import com.example.demo.logics.TblUserLogic;
-import com.example.demo.utils.Constant;
 
 /**
  * @author HP
--- src/main/java/com/example/demo/daos/impls/TblCompanyDaoImpl.java
+++ src/main/java/com/example/demo/daos/impls/TblCompanyDaoImpl.java
@@ -13,7 +13,6 @@ import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.example.demo.daos.TblCompanyDao;
 import com.example.demo.daos.TblCompanyDaoCustom;
 import com.example.demo.entities.Company;
 import com.example.demo.entities.TblCompany;
